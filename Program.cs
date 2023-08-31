69e14191b621219c18fb6c9f6308610529372afb
Luca Milan <luca_milan@hotmail.com>
2023-08-31 12:03:35 +0200

    Calling api

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
+var environment = "";
 var builder = WebApplication.CreateBuilder(args);
+var apiEndpoint = builder.Configuration["ApiEndpoint"];
+
+builder.Services.AddSingleton(sp=>{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    return new ApiClient(configuration["ApiEndpoint"]!);
+});
+
 var app = builder.Build();
+
+environment = app.Environment.EnvironmentName;
 
 app.MapGet("/", () => PrintHtml("<h1>This is the most simple App in the World</h1>"));
 
-app.MapGet("/feature", () => PrintHtml("<h1>I'm calling \"The Most simple Api\"</h1>"));
+app.MapGet("/feature", async (ApiClient apiClient) => PrintHtml("<h1>" + await apiClient.Get() + "</h1>"));
 
 app.Run();
 
-static IResult PrintHtml(params string[] fragments){
+IResult PrintHtml(params string[] fragments){
     const string html = @"<html><head><title>The Most Simple App</title><style>@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');@import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
     body {{
         font-family: 'Muli', sans-serif;
@@ -38,8 +48,28 @@ static IResult PrintHtml(params string[] fragments){
             color: rgba(0, 0, 0, 0.8);
             -webkit-font-smoothing: antialiased;
             }}
-        </style></head></body><div><a href='/'>Back to home</a> - <a href='/feature'>The most rilevant Feature</a></div>{0}
-        <div><h3>By @Luca Milan {1}</h3></div>
+        </style></head></body><div><a href='/'>Back to home</a> - <a href='/feature'>Ask me all you want</a></div>{0}
+        <div><h3>By @Luca Milan {1}  [Enviroment: {2}]</h3></div>
         </body></html>";   
-    return Results.Content(string.Format(html, string.Join(" ",fragments), DateTime.UtcNow.ToString("u")), "text/html");
+    return Results.Content(string.Format(html, string.Join(" ",fragments), DateTime.UtcNow.ToString("u"),environment), "text/html");
+}
+
+class ApiClient{
+    private static readonly HttpClient _httpClient = new();
+
+    public ApiClient(string endpoint)
+    {
+        if (string.IsNullOrWhiteSpace(endpoint))
+        {
+            throw new ArgumentException($"'{nameof(endpoint)}' cannot be null or whitespace.", nameof(endpoint));
+        }
+
+        _httpClient.BaseAddress = new Uri(endpoint);
+        _httpClient.Timeout = new TimeSpan(0, 0, 30);
+    }
+    public async Task<string> Get(){
+        var response = await _httpClient.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
 }
